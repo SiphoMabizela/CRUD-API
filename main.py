1de76a78df052d95c2d2ad9044ae6576594aab47
Sipho Mabizela <smabizela3@gmail.com>
2026-08-17 14:50:54 +0200

    Stage 2: Added read endpoints, amended error exception with 404.

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+
+# in-memory task list
+tasks = [
+    {
+        "id": 1,
+        "title": "Do the laundry.",
+        "done": False
+    },
+    {
+        "id": 2,
+        "title": "Write my CCNA exam.",
+        "done": False
+    },
+    {
+        "id": 3,
+        "title": "Enroll to FlyRank internship.",
+        "done": True
+    }
+]
 
 
 @app.get("/")
@@ -15,3 +35,20 @@ def read_root():
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return tasks
+
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+
+    raise HTTPException(
+    status_code=404,
+    detail=f"Task {task_id} not found"
+)
