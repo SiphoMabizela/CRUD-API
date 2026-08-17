33e21d65f02edcc8975f34c1dde5dbbc21ee0fbc
Sipho Mabizela <smabizela3@gmail.com>
2026-08-17 16:15:23 +0200

    Stage 4: Update and delete endpoints added, functionality tested on Swagger UI.

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ class TaskCreate(BaseModel):
     title: str | None = None
 
 
+class TaskUpdate(BaseModel):
+    title: str | None = None
+    done: bool | None = None
+
+
 # In-memory task list
 tasks = [
     {
@@ -64,7 +69,7 @@ def create_task(task: TaskCreate):
     if not task.title or not task.title.strip():
         raise HTTPException(
             status_code=400,
-            detail="Title is required and cannot be empty"
+            detail="Title is required and cannot be left empty"
         )
 
     new_id = max(task["id"] for task in tasks) + 1
@@ -78,3 +83,41 @@ def create_task(task: TaskCreate):
     tasks.append(new_task)
 
     return new_task
+
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, task_update: TaskUpdate):
+    for task in tasks:
+        if task["id"] == task_id:
+
+            if task_update.title is not None:
+                if not task_update.title.strip():
+                    raise HTTPException(
+                        status_code=400,
+                        detail="Title cannot be left empty"
+                    )
+
+                task["title"] = task_update.title
+
+            if task_update.done is not None:
+                task["done"] = task_update.done
+
+            return task
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
+
+
+@app.delete("/tasks/{task_id}", status_code=204)
+def delete_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            tasks.remove(task)
+            return
+
+    raise HTTPException(
+        status_code=404,
+        detail=f"Task {task_id} not found"
+    )
